128d792981bfdb7a1e8162dab9f43cf8049bd837
times29 <tds29@posteo.net>
2023-02-02 23:11:22 +0100

    Pass existing category to category modal

--- src/app/category/category-modal/category-modal.component.ts
+++ src/app/category/category-modal/category-modal.component.ts
@@ -1,24 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Category } from '../../shared/domain';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-category-modal',
   templateUrl: './category-modal.component.html',
 })
-export class CategoryModalComponent {
-  isCreate!: boolean;
+export class CategoryModalComponent implements OnInit {
+  // Passed into the component by the ModalController, available in the ngOnInit
+  category!: Category;
 
-  constructor(private readonly modalCtrl: ModalController) {}
+  readonly categoryForm: FormGroup;
 
-  cancel = (): void => {
+  constructor(private readonly formBuilder: FormBuilder, readonly modalCtrl: ModalController) {
+    this.categoryForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(40)]],
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.category) this.categoryForm.patchValue(this.category);
+  }
+
+  cancel(): void {
     this.modalCtrl.dismiss(null, 'cancel');
-  };
+  }
 
-  save = (): void => {
-    this.modalCtrl.dismiss(null, 'confirm');
-  };
+  save(): void {
+    this.modalCtrl.dismiss(this.categoryForm.value, 'confirm');
+  }
 
-  delete = (): void => {
+  delete(): void {
     this.modalCtrl.dismiss(null, 'delete');
-  };
+  }
 }
--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -4,9 +4,10 @@ import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryRoutingModule } from './category-routing.module';
 import { IonicModule } from '@ionic/angular';
 import { CategoryModalComponent } from './category-modal/category-modal.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryModalComponent],
-  imports: [CommonModule, CategoryRoutingModule, IonicModule],
+  imports: [CommonModule, CategoryRoutingModule, IonicModule, ReactiveFormsModule],
 })
 export class CategoryModule {}
